299c31ccc965dab8c4ce11d1fb29ecddd353ad60
Cen Wang <iwarobots@gmail.com>
2014-01-16 18:21:40 +0800

    beta

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def main():
-    resp = download_imdb_top_page()
-    parse_page(resp)
+def str2(navigable_string, src_encoding=None, dst_encoding=None):
+    if src_encoding is None:
+        src_encoding = 'utf-8'
+    if dst_encoding is None:
+        dst_encoding = 'utf-8'
+    return navigable_string.encode(src_encoding).decode(dst_encoding)
 
 
-def download_imdb_top_page():
+def download_imdb_top_250_page():
     url = 'http://www.imdb.com/chart/top'
     return requests.get(url)
 
@@ -24,9 +27,20 @@ def parse_page(resp):
         td = tr.find('td', {'class': 'titleColumn'})
         if td is not None:
             a = td.find('a')
-            movie = a.string
+            movie = str2(a.string)
             movies.append(movie)
     return movies
+
+
+def print_movies(movies):
+    for movie in movies:
+        print(movie)
+
+
+def main():
+    resp = download_imdb_top_250_page()
+    movies = parse_page(resp)
+    print_movies(movies)
 
 
 if __name__ == '__main__':
